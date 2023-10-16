75eb59dc8c1bef4604bfb6a398d91d482b24a1c5
Santos Alarcón Asensio <santosalarcon86@gmail.com>
2023-10-16 11:18:41 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://santosalarcon.github.io",
+  base: "/guia-neovim"
+});
