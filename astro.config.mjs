95f6b2bf5b965a2db1782bb4c6351351f5c3de07
Santos Alarcón <santosalarcon86@gmail.com>
2023-10-17 10:50:36 +0200

    Configured base and site in astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,8 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://santosalarcon.github.io",
+  base: "/guia-neovim/",
   output: "static",
   publicDir: "./public",
   redirects: {
