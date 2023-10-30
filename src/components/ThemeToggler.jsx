982f9100039018d865e29604e55a7f9000017655
Santos Alarcón <santosalarcon86@gmail.com>
2023-10-30 19:46:19 +0100

    Arreglado selector de temas

--- src/components/ThemeToggler.jsx
+++ src/components/ThemeToggler.jsx
@@ -1,61 +1,67 @@
-import React, { useEffect, useState } from "react"
+import { useEffect, useState } from "react"
 import IconoSol from "./IconoSol"
 import IconoLuna from "./IconoLuna"
 
 const ThemeToggler = () => {
-    const themes = ["light", "dark"]
-    const [theme, setTheme] = useState(() => {
-        if (import.meta.env.SSR) {
-            return undefined
-        }
-
-        if (typeof localStorage !== undefined && localStorage.getItem("theme")) {
-            return localStorage.getItem("theme")
-        }
-
-        // Si encuentra la propiedad "dark" en el "prefers-color-scheme", devuelve
-        // "dark".
-        if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
-            return "dark"
-        }
-
-        return "light"
-    })
-    
-    const changeTheme = () => {
-        const t = theme === "light" ? "dark" : "light"
-        localStorage.setItem("theme", t)
-        setTheme(t)
+  const [mounted, setMounted] = useState(false)
+  const [theme, setTheme] = useState(() => {
+    if (import.meta.env.SSR) {
+      return undefined
     }
 
-    // Si no hay ninguna clave en el LocalStorage se crea una. Además se cambia el
-    // esquema de colores.
-    useEffect(() => {
-        if (!localStorage.getItem("theme")) {
-            localStorage.setItem("theme", theme)
-        }
+    // Se comprueba si hay una clave en el LocalStorage. En caso de que la haya
+    // recupera el valor.
+    if (typeof localStorage !== undefined && localStorage.getItem("theme")) {
+      return localStorage.getItem("theme")
+    }
 
-        document.documentElement.setAttribute("theme", theme)
-    }, [theme])
+    // Si encuentra la propiedad "dark" en el "prefers-color-scheme", devuelve
+    // "dark".
+    if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+      return "dark"
+    }
 
+    // Devuelve "light" en caso de que los casos anteriores no se cumplan.
+    return "light"
+  })
 
-    return (
-        <div id="theme-toggle">
-            {themes.map(t => {
-                const checked = t === theme;
-                return (
-                    <button
-                        aria-label="Cambiar tema"
-                        className={checked ? "checked" : null}
-                        key={t}
-                        onClick={changeTheme}
-                    >
-                        {t === "light" ? <IconoSol /> : <IconoLuna />}
-                    </button>
-                )
-            })}
-        </div>
+  const changeTheme = () => {
+    console.log(`Color scheme: ${theme}`)
+    const t = theme === "light" ? "dark" : "light"
+    localStorage.setItem("theme", t)
+    setTheme(t)
+  }
+
+  // Si no hay ninguna clave en el LocalStorage se crea una. Además se cambia el
+  // esquema de colores, añadiendo el atributo "theme" al HTML.
+  useEffect(() => {
+    if (!localStorage.getItem("theme")) {
+      localStorage.setItem("theme", theme)
+    }
+
+    document.documentElement.setAttribute("theme", theme)
+  }, [theme])
+
+  useEffect(() => {
+    setMounted(true)
+  }, [])
+
+  return (
+    mounted && (
+      <div
+        id="theme-toggle"
+        className={theme === "dark" ? "checked" : ""}
+        onClick={changeTheme}
+      >
+        <i
+          title={theme === "dark" ? "Modo oscuro" : "Modo claro"}
+          className={`dot ${theme === "dark" ? "checked" : ""}`}
+        >
+          {theme === "light" ? <IconoSol /> : <IconoLuna />}
+        </i>
+      </div>
     )
+  )
 }
 
 export default ThemeToggler
