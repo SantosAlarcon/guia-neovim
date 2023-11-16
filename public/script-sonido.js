0a700c6ca52859aba9eefcc8cc72eec1b6acd189
= <=>
2023-11-16 13:10:42 +0100

    Arreglada la ejecución del componente de sonido tras la navegación

--- public/script-sonido.js
+++ public/script-sonido.js
@@ -1,7 +1,21 @@
-const sonido = new Audio("/guia-neovim/tecla.mp3");
-const teclas = document.querySelectorAll("kbd");
-teclas.forEach((tecla) => {
-	tecla.addEventListener("click", () => {
-		sonido.play();
+const SonidoTeclado = () => {
+	const sonido = new Audio("/guia-neovim/tecla.mp3");
+	const teclas = document.querySelectorAll("kbd");
+	teclas.forEach((tecla) => {
+		tecla.addEventListener("click", () => {
+			sonido.play();
+		});
 	});
+};
+
+document.addEventListener(
+	"astro:page-load",
+	() => {
+		SonidoTeclado();
+	},
+	{ once: true },
+);
+
+document.addEventListener("astro:after-swap", () => {
+	SonidoTeclado();
 });
