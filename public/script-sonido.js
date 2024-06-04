788c0efc345177234c3190a31b9cd3a3d29c2659
Santos Alarcón <santosalarcon86@gmail.com>
2024-06-04 18:55:10 +0200

    Dependencias actualizadas Junio 2024

--- public/script-sonido.js
+++ public/script-sonido.js
@@ -1,7 +1,24 @@
-const sonido = new Audio("/guia-neovim/tecla.mp3");
-const teclas = document.querySelectorAll("kbd");
-teclas.forEach((tecla) => {
-	tecla.addEventListener("click", () => {
-		sonido.play();
-	});
-});
+const SonidoTecla = async () => {
+  const sonido = new Audio("/guia-neovim/Tecla.mp3")
+  const teclas = document.querySelectorAll("kbd")
+  teclas.forEach((tecla) => {
+    tecla.addEventListener("click", () => {
+      sonido.play()
+    })
+  })
+}
+
+document.addEventListener(
+  "astro:page-load",
+  () => {
+    SonidoTecla()
+  },
+  { once: true },
+)
+
+document.addEventListener(
+  "astro:after-swap",
+  () => {
+    SonidoTecla()
+  }
+)
