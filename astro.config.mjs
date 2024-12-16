ae11cfe0663b44ec4e6df316d8d659faa828a327
Linux User <futuzor@FUTUZOR-W11.>
2024-12-16 19:39:26 +0100

    fix(details): non smooth open/close animation

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
-import { defineConfig } from 'astro/config';
-import preact from '@astrojs/preact';
+import { defineConfig } from "astro/config";
+import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://santosalarcon.github.io",
-  base: "/guia-neovim/",
-  output: "static",
-  publicDir: "./public",
-  integrations: [preact()]
+	site: "https://santosalarcon.github.io",
+	base: "/guia-neovim/",
+	output: "static",
+	publicDir: "./public",
+	integrations: [preact()],
 });
