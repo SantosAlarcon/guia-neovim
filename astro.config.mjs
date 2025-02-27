62a5c8efcf600c065c3c097cf1aab31dcfdd6844
= <=>
2025-02-27 13:43:15 +0100

    seo: configuracion sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,15 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://www.guia-neovim.es",
-	//base: "/guia-neovim/",
-	output: "static",
-	publicDir: "./public",
-	integrations: [preact(), sitemap()],
+    site: "https://www.guia-neovim.es",
+    //base: "/guia-neovim/",
+    output: "static",
+    publicDir: "./public",
+    integrations: [preact(), sitemap({
+	changefreq: "daily",
+	priority: 0.8,
+	lastmod: new Date(),
+    })],
     prefetch: {
         prefetchAll: true,
     },
