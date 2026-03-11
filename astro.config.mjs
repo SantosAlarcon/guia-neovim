42442cb9b0b8c72c2c1eaad6ee6a87b46a837e4f
= <=>
2026-03-11 11:47:51 +0100

    chore: cambio a Astro 6 y fuentes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,14 @@
-import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
+	experimental: {
+		rustCompiler: true,
+		queuedRendering: {
+			enabled: true,
+		},
+	},
 	site: "https://www.guia-neovim.es",
 	//base: "/guia-neovim/",
 	output: "static",
